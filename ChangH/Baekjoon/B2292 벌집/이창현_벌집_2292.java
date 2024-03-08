fcc562ac555db4371e96fe931c07fcd9bd87fe04
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2024-03-08 15:20:57 +0900

    [백준]벌집_2292

--- ChangH/Baekjoon/B2292 벌집/이창현_벌집_2292.java
+++ ChangH/Baekjoon/B2292 벌집/이창현_벌집_2292.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class 이창현_벌집_2292 {
 	static Scanner sc = new Scanner(System.in);
-	static int N, result;
-	static int nowMax = 1, cnt = 1;
-	static int increment = 6;
+	static int N, result; //N과 결과값
+	static int nowMax = 1, cnt = 1; //nowmax==현재 벌집 한계치, cnt 증설된 벌집 층수
+	static int increment = 6; //증설 기준치
 	
 	public static void main(String[] args) {
-		test();
+		test(); //테스트
 	}
 
 	private static void test() {
-		testInput();
-		testProcess();
-		testOutput();
+		testInput(); //입력
+		testProcess(); //실행
+		testOutput(); //출력
 	}
 
 	private static void testInput() {
-		N=sc.nextInt();
+		N=sc.nextInt(); // 입력
 	}
 
 	private static void testProcess() {
@@ -27,13 +27,13 @@ public class 이창현_벌집_2292 {
 			if(N>nowMax) { //벌집 cnt 층안에 N이 있을때까지
 				nowMax += increment *cnt++; //벌집 확장
 			} else {
-				result = cnt; //있으면 반환
+				result = cnt; //있으면 반환 후 종료
 				return;
 			}
 		}
 	}
 
 	private static void testOutput() {
-		System.out.println(result);
+		System.out.println(result); //출력
 	}
 }
